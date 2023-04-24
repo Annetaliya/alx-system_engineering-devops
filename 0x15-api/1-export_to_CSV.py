139b5c008dd2915097b8c4f953e0107215a7b0fb
Annette Adhiambo <annetaliya@gmail.com>
2023-04-24 09:57:09 -0700

    cdv export

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' extend your Python script to export data in the CSV format '''
+
+import csv
+import requests
+import sys
+
+if __name__ == '__main__':
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/" + user_id
+    json_dict = requests.get(url).json()
+    name_of = json_dict.get("username")
+    user_task = requests.get("{}/todos".format(url))
+    user_task = user_task.json()
+    file_name = user_id + '.csv'
+
+    with open(file_name, 'w') as csvfile:
+        for items in user_task:
+            csvfile.write('"{},"{}","{}","{}"\n'.format(items.get(
+                "userId"), name_of, items.get("completed"),
+                items.get("title")))
